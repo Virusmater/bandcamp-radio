c6c876fbda0bc137aba5d23a9a8a2172970f1be8
Virusmater <virusmater@gmail.com>
2020-05-03 15:47:13 +0200

    add new methods to the api, probably need to move it to separate package

--- bandcamp_radio/bandcamp.py
+++ bandcamp_radio/bandcamp.py
@@ -1,5 +1,21 @@
 import requests
 import json
+from html.parser import HTMLParser
+import time
+
+
+class _FanIdParser(HTMLParser):
+    def error(self, message):
+        pass
+
+    fan_id = ""
+
+    def handle_starttag(self, tag, attrs):
+        for attr in attrs:
+            if attr[0] == "data-blob":
+                data_html = attr[1]
+                data = json.loads(data_html)
+                self.fan_id = data['fan_data']['fan_id']
 
 
 def discover(genre="all", sub_genre="any", slice="best", page=0):
@@ -8,3 +24,24 @@ def discover(genre="all", sub_genre="any", slice="best", page=0):
     json.loads(request.content)
     print("got", genre, sub_genre, slice)
     return json.loads(request.content)['items']
+
+
+def get_fan_id(name):
+    url = F"https://bandcamp.com/{name}"
+    request = requests.get(url)
+    parser = _FanIdParser()
+    content = request.content
+    parser.feed(content.decode('utf-8'))
+    return parser.fan_id
+
+
+def get_collection(fan_id, count=1000):
+    url = "https://bandcamp.com/api/fancollection/1/collection_items"
+    token = get_token()
+    body = F'{{"fan_id": "{fan_id}", "older_than_token": "{token}", "count":"{count}"}}'
+    x = requests.post(url, data=body)
+    return json.loads(x.text)['items']
+
+
+def get_token():
+    return str(int(time.time())) + "::FOO::"
